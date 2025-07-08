bb4fb6e5dff8d6f009b40946e5df4bcf332bde86
Safal Sharma <safalsharma1991.ss@gmail.com>
2025-07-08 17:51:19 +0530

    Add admin orders view and link

--- models.py
+++ models.py
@@ -52,6 +52,7 @@ class OrderItem(Base):
     __tablename__ = "order_items"
     id = Column(Integer, primary_key=True)
     order_id = Column(Integer, ForeignKey("orders.id"))
-    product_id = Column(Integer)
+    product_id = Column(Integer, ForeignKey("products.id"))
     quantity = Column(Integer)
     order = relationship("Order", back_populates="items")
+    product = relationship("Product")
