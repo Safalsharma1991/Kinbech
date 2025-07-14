ac1e00d344069db220f5a9ffd203c0904674155c
Safal Sharma <safalsharma1991.ss@gmail.com>
2025-07-14 16:17:06 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,18 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Base
-import os
 
-# Fetch the database URL from the environment variable
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///test.db")
+SQLALCHEMY_DATABASE_URL = "postgresql://kinbech_database_user:mi55OxsKZfBm83ziODhz9joYmIsw4O1s@dpg-d1qdci3e5dus73e59sbg-a.oregon-postgres.render.com/kinbech_database"
 
-# If the database URL is for PostgreSQL, use it directly (no need to replace 'postgres://')
-if SQLALCHEMY_DATABASE_URL.startswith("postgresql://"):
-    engine = create_engine(SQLALCHEMY_DATABASE_URL)
-else:
-    # For SQLite or other databases (handled here just in case)
-    engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
