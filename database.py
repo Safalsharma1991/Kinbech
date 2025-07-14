e29fc94cf5396a3b2fe803cd9cc8952b86795f09
Safal Sharma <safalsharma1991.ss@gmail.com>
2025-07-14 16:34:26 +0530

    Update database.py

--- database.py
+++ database.py
@@ -6,6 +6,7 @@ SQLALCHEMY_DATABASE_URL = "postgresql://kinbech_database_user:mi55OxsKZfBm83ziOD
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
